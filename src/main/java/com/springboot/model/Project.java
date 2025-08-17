66c6d430f39c94fb749c1ca04b4c2706692d7964
Red-Opera <hyunsim01@naver.com>
2025-08-18 00:07:21 +0900

    출시 예정 프로젝트 추가, 출시 예정 프레임 디자인 추가

--- src/main/java/com/springboot/model/Project.java
+++ src/main/java/com/springboot/model/Project.java
@@ -4,11 +4,20 @@ public class Project
 {
     private String title;
     private String description;
+    private String releaseStatus;
 
     public Project(String title, String description) 
     {
         this.title = title;
         this.description = description;
+        this.releaseStatus = "released";
+    }
+
+    public Project(String title, String description, String releaseStatus) 
+    {
+        this.title = title;
+        this.description = description;
+        this.releaseStatus = releaseStatus;
     }
 
     public String getTitle() 
@@ -29,5 +38,15 @@ public class Project
     public void setDescription(String description) 
     {
         this.description = description;
+    }
+
+    public String getReleaseStatus() 
+    {
+        return releaseStatus;
+    }
+
+    public void setReleaseStatus(String releaseStatus) 
+    {
+        this.releaseStatus = releaseStatus;
     }
 } 
